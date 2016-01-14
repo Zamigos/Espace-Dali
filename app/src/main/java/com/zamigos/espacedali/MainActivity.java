acd2530950c7be3c928755892ffa14bf7e19327b
Zagonine <hello@zagonine.io>
2016-01-14 10:52:40 +0100

    Deleted Menu Setting in ActionBar

--- app/src/main/java/com/zamigos/espacedali/MainActivity.java
+++ app/src/main/java/com/zamigos/espacedali/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -63,11 +62,6 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
