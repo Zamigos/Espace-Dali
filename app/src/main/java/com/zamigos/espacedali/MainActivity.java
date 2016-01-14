aa657bf4e44b85565845e8f4d423126eb5a48deb
001matt <pringuez.matthieu@gmail.com>
2016-01-14 13:57:10 +0100

    Slpash screen + fixture

--- app/src/main/java/com/zamigos/espacedali/MainActivity.java
+++ app/src/main/java/com/zamigos/espacedali/MainActivity.java
@@ -3,6 +3,7 @@ package com.zamigos.espacedali;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,7 +20,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
